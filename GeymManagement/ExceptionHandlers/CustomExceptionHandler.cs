d5cb412ae7b9d8310027c1b78fed0381e6d1dbf0
Khader Mohamed <khaderelhabal923@gmail.com>
2026-05-16 22:59:09 +0300

    feat(exception-handling): implement global exception handler with custom error view

---
+++ GeymManagement/ExceptionHandlers/CustomExceptionHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace GymManagement.Presentation.ExceptionHandlers;
+
+public class CustomExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<CustomExceptionHandler> _logger;
+
+    public CustomExceptionHandler(ILogger<CustomExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+     
+        _logger.LogError(exception, "An unhandled exception occurred: {Message}", exception.Message);
+
+
+        return false;
+    }
+}
