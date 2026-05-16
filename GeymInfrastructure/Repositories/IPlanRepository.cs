f53652bb258a1448098eacc611a8e640f3738551
Khader Mohamed <khaderelhabal923@gmail.com>
2026-05-16 22:54:03 +0300

    feat(infrastructure): implement repository pattern for data access abstraction

---
+++ GeymInfrastructure/Repositories/IPlanRepository.cs
@@ -0,0 +1,13 @@
+using GymManagement.Infrastructure.Models;
+
+namespace GeymInfrastructure.Repositories;  
+  
+public interface IPlanRepository  
+{  
+    Task<IEnumerable<Plan>> GetAllAsync();  
+    Task<Plan?> GetPlanById(int id);  
+    void Add(Plan plan);  
+    void Update(Plan plan);  
+    void Delete(Plan plan);  
+    Task<int> SaveChangesAsync();  
+} 
