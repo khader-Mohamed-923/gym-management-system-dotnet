8615e0fe3d62bc60489065f21c7314de9981d9ce
Khader Mohamed <khaderelhabal923@gmail.com>
2026-05-31 20:46:41 +0300

    Refactor: update dependency injection configuration in Domain

--- GeymDomain/DependencyInjection.cs
+++ GeymDomain/DependencyInjection.cs
@@ -12,26 +12,22 @@ public static class DependencyInjection
         this IServiceCollection services,
         IConfiguration configuration)
     {
-        // 1. تسجيل خدمات الـ Infrastructure كالعادة
         services.AddInfrastructure(configuration);
 
-        // 2. كود الـ Seeding الأوتوماتيكي والاحترافي
-        // بنبني ServiceProvider مؤقت عشان نعرف نطلع منه الـ DbContext
+
         var serviceProvider = services.BuildServiceProvider();
 
         using (var scope = serviceProvider.CreateScope())
         {
             try
             {
-                // بنجيب الـ DbContext من الـ Scope
+              
                 var context = scope.ServiceProvider.GetRequiredService<GymDbContext>();
 
-                // بننادي على الـ Seeder بتاعك (وبنحولها لـ Synchronous عشان إحنا جوه ميثود static مش async)
                 DatabaseSeeder.Seed(context).GetAwaiter().GetResult();
             }
             catch (Exception ex)
             {
-                // لو حصل أي Error في الـ Seeding، البرنامج مش هيقفل، وهيعدي عادي
                 Console.WriteLine($"Seeding Failed: {ex.Message}");
             }
         }
