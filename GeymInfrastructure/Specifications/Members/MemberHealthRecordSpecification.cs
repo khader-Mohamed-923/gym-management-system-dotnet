7483ce61608a7f7aaf5f3dd80beba57b66124562
Khader Mohamed <khaderelhabal923@gmail.com>
2026-06-05 18:41:10 +0300

    feat(specs): introduce specification pattern

---
+++ GeymInfrastructure/Specifications/Members/MemberHealthRecordSpecification.cs
@@ -0,0 +1,14 @@
+﻿
+
+using GymManagement.Infrastructure.Models;
+
+namespace GymManagement.Infrastructure.Specifications.Members;
+
+public class MemberHealthRecordSpecification : BaseSpecification<Member>
+{
+    public MemberHealthRecordSpecification(int memberId)
+        : base(m => m.Id == memberId)
+    {
+        AddInclude(m => m.HealthRecord);
+    }
+}
