fb4f83d622be9e5c2e9102b09c038be49e909d4b
Khader Mohamed <khaderelhabal923@gmail.com>
2026-06-07 06:11:30 +0300

    fix: Add Fitness value to Speciality enum to map existing DB records

--- GeymInfrastructure/Enums/Speciality.cs
+++ GeymInfrastructure/Enums/Speciality.cs
@@ -1,14 +1,14 @@
-﻿
+
 
 namespace GymManagement.Infrastructure.Enums;
 
 public enum Speciality
 {
 
-    Cardio =1,
-    Balance =2,
-    Boxing =3,
-    Yoga =4,
-    CrossFit =5
-
+    Cardio = 1,
+    Balance = 2,
+    Boxing = 3,
+    Yoga = 4,
+    CrossFit = 5,
+    Fitness = 6
 }
