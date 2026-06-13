489c9923b94a1d0e66a3738c948a6685f3567df8
Khader Mohamed <khaderelhabal923@gmail.com>
2026-06-13 21:27:25 +0300

    feat: add ViewModels to Presentation layer

---
+++ GeymManagement/ViewModels/Member/MemberDetailsViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace GymManagement.Presentation.ViewModels.Member;
+
+public class MemberDetailsViewModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Phone { get; set; }= null!;
+
+    public string? PhotoUrl { get; set; }
+
+
+    public string Gender { get; set; }= null!;
+
+    public string Address { get; set; } = null!;
+
+    public string DateOfBirth { get; set; } = null!;
+    public string PlanName { get; set; } = null!;
+
+    public string MembershipStartDate { get; set; } = null!;
+
+    public string MembershipEndDate { get; set;} = null!;
+
+
+}
---
+++ GeymManagement/ViewModels/Member/MemberIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace GymManagement.Presentation.ViewModels.Member;
+
+public class MemberIndexViewModel
+{
+    public int Id { get; set; }
+
+    public string  Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+
+    public string? Gender { get; set; }
+
+    public string? PhotoPath { get; set; }
+
+    public DateOnly JoinDate { get; set; } 
+
+
+
+
+}
